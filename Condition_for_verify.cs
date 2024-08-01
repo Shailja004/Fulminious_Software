fa8a41259beed5be526911537238c27db1bd2d11
Shailja004 <shailjabhargava2004@gmail.com>
2024-08-01 09:38:45 +0530

    Customer's_Record

---
+++ Condition_for_verify.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerInfo
+{
+    public class Condition : IVerifier
+    {
+        public bool IsValid(Customer customer)
+        {
+            if (customer.CustomerAge < 18)
+            {
+                Console.WriteLine("Customer is not eligible.");
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
---
+++ Store_Customer_Info.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerInfo
+{
+    public class Store_Information : ISave_information
+    {
+        string _filePath;
+        public Store_Information(string File)
+        {
+            _filePath = File;
+        }
+        public void SaveCustomers(List<Customer> customers)
+        {
+            {
+                foreach (var customer in customers)
+                {
+                   File.AppendAllText("customer.txt", customer.CustomerName + "  " + customer.CustomerPhone + "  " + customer.CustomerEmail + "  " + customer.CustomerAge + "\n");
+                }
+            }
+        }
+    }
+}
